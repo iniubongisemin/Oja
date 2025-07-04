9ab24f8040721919ac8a252d23f9bf857eee225c
iniubongisemin <eugeneinie@gmail.com>
2025-07-04 03:42:27 +0100

    FEAT: Added delete feature

--- OJA/OJA/urls.py
+++ OJA/OJA/urls.py
@@ -4,7 +4,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("items/", include("item.urls")),
     path("", include("core.urls")),
+    path("items/", include("item.urls")),
+    path("dashboard/", include("dashboard.urls")),
+    path("admin/", admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) # NEVER DO THIS IN PROD!!!! 😱
